d002b70b719d93269dc4f9cb67ff3cb70a61f6a6
veko <vekonyat@gmail.com>
2024-04-27 22:42:29 +0200

    Körök, tipp, részeredmények hozzáadva

--- src/csigaverseny/Csiga.java
+++ src/csigaverseny/Csiga.java
@@ -1,14 +1,36 @@
 
 package csigaverseny;
 
+import java.util.Random;
+
 
 public class Csiga {
     
+    private static Random rnd = new Random();
     private String szin;
-    private int sebsseg;
-    private int messze;
+    private int sebesseg;
+    private int messze = 0;
+    private int messzeOld = 0;
     private boolean kapGyorsitot;
+    private int maxSeb = 3;
     
     public static String[] szinek = {"piros", "zold", "kek"};
+    
+    public void setSebesseg() {
+        sebesseg = rnd.nextInt(maxSeb)+1;
+    }
+    
+    public int getSebesseg() {
+        return sebesseg;
+    }
+    
+    public void setMessze() {
+        messze = messzeOld + sebesseg;
+        messzeOld = messze;
+    }
+
+    public int getMessze() {
+        return messze;
+    }
     
 }
--- src/csigaverseny/Verseny.java
+++ src/csigaverseny/Verseny.java
@@ -4,6 +4,7 @@
  */
 package csigaverseny;
 
+import java.util.Arrays;
 import java.util.Random;
 
 /**
@@ -14,6 +15,11 @@ public class Verseny {
 
     private static Random rnd = new Random();
     private static int korok = 5;
+    private int[] messze = {0, 0, 0}; 
+    int[] sebesseg;
+    int seb1, seb2, seb3;
+   
+    
     public Verseny() {
         kezdes();
     }
@@ -24,12 +30,28 @@ public class Verseny {
     Csiga csiga2 = new Csiga();
     Csiga csiga3 = new Csiga();
         
-    int fogad = Verseny.rnd.nextInt(3);    
+    int fogad = Verseny.rnd.nextInt(3);
+    
+    csiga1.setSebesseg();
+    csiga2.setSebesseg();
+    csiga3.setSebesseg();
+           
+    seb1 = csiga1.getSebesseg();
+    seb2 = csiga2.getSebesseg();
+    seb3 = csiga3.getSebesseg();
+    
+    sebesseg = new int[] {seb1, seb2, seb3};
     
     System.out.println("Fogadás: " + Csiga.szinek[fogad]);   
+    System.out.println("Csigák sebessége: " + Arrays.toString(sebesseg));
     
         for (int i = 0; i < korok; i++) {
-            System.out.println(i);
+            System.out.println("Körszám: " + i);
+             csiga1.setMessze();
+             csiga2.setMessze();
+             csiga3.setMessze();
+             messze = new int[] {csiga1.getMessze(), csiga2.getMessze(), csiga3.getMessze()}; 
+            System.out.println("Távolság: " + Arrays.toString(messze));
         }
     
     }
